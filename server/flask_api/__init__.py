47b405daafc46b1b43f1d70c366f48fb540adabd
Jaxon Desktop <handtjaxon@gmail.com>
2022-04-22 14:58:10 -0500

    Renamed 'server_api' to the new 'server' folder

--- server/flask_api/__init__.py
+++ server/flask_api/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask, session
 app = Flask(__name__)
+
+# Could add this to environment vars in a .env file after importing the dotenv package?
 # need to move to a non-github folder
 app.secret_key = "shhhhhh"
