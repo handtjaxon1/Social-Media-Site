5df105317a703a8243f497015c6b6137715f4ad1
Jaxon Handt <handtjaxon@gmail.com>
2022-04-26 23:21:02 -0500

    Created a new layout component to give form to the main content of all pages

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,13 @@ import Posts from "./components/posts/Posts";
 import Home from "./components/Home";
 import Navigation from "./components/nav/Navigation";
 import AddPost from "./components/posts/AddPost";
+import EditPost from "./components/posts/EditPost";
+import ViewPost from "./components/posts/ViewPost";
 import Profile from "./components/Profile";
 import NoMatch from "./components/NoMatch";
+import Layout from "./components/Layout";
 import { CLIENT_URLS } from "./constants/clientRoutes";
+
 
 function App() {
   // TODO Refactor loggin based on actual authentication
@@ -17,12 +21,18 @@ function App() {
     <BrowserRouter>
       <Navigation isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
       <Routes>
-        <Route index element={<Home />} />
-        <Route path={CLIENT_URLS.auth} element={<Auth />} />
-        <Route path={CLIENT_URLS.posts} element={<Posts />} />
-        <Route path={CLIENT_URLS.add_post} element={<AddPost />} />
-        <Route path={CLIENT_URLS.profile} element={<Profile />} />
-        <Route path={CLIENT_URLS.no_match} element={<NoMatch />} />
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path={CLIENT_URLS.auth} element={<Auth />} />
+          <Route path={CLIENT_URLS.profile} element={<Profile />} />
+
+          <Route path={CLIENT_URLS.posts} element={<Posts />} />
+          <Route path={CLIENT_URLS.add_post} element={<AddPost />} />
+          <Route path={CLIENT_URLS.edit_post} element={<EditPost />} />
+          <Route path={CLIENT_URLS.view_post} element={<ViewPost />} />
+
+          <Route path={CLIENT_URLS.no_match} element={<NoMatch />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/components/Layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Container } from "reactstrap";
+
+// This component serves purely as a "wrapper" around the main page content
+export default function Layout(props) {
+    return (
+        <Container className="py-3">
+            <Outlet />
+        </Container>
+    );
+}
